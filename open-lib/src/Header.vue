f8084481b3bb03e6ccffaafbbd428b038da40163
lgh06 <hnnk@qq.com>
2016-12-06 11:19:06 +0800

    test begin

---
+++ open-lib/src/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <div class="aaa">111</div>
+        <div class="aaa">222</div>
+        <div class="aaa">333</div>
+    </header>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
